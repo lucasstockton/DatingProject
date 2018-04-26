e654b325a034a19ffd50a0d0295f2ecc478f022e
lucasstockton <35933381+lucasstockton@users.noreply.github.com>
2018-04-26 17:51:56 -0400

    asdfasdf
    sdfsadf

---
+++ DatingProject/Data/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingProject.Data
+{
+    public class IUserRepository
+    {
+        void Add<T>(T entity) where T : 
+
+    }
+}
